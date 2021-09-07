7b461dd8c15583d8c40eb58ac7aab623f15ee2a1
Zain-IIU <xainulabideen600@gmail.com>
2021-09-07 15:57:42 +0500

    Added Custom Event System for Giant Enemy

---
+++ MLAB/Assets/Scripts/Enemy/GiantController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantController : MonoBehaviour
+{
+    Animator Anim;
+    [SerializeField]
+    int ID;
+    private void Awake()
+    {
+        Anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.instance.OnPlayerEnter += ActivateAnimator;
+    }
+
+   void ActivateAnimator(int id)
+    {
+        if(id==this.ID)
+         Anim.enabled = true;
+    }
+}
